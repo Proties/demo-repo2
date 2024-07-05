9689dd265ece00cef9c5c5f078bd2577405e9bd6
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-07-05 22:56:17 +0200

    updates

--- php/rank.php
+++ php/rank.php
@@ -6,16 +6,21 @@ class Ranking extends Database{
 
 
 public function chrono(Array $arr){
+    $p='';
     try{
-
         $db=$this->get_connection();
+        $len=count($arr);
+        for($i=0;$i<$len;$i++){
+            $p.='AND userID='.$arr[$i];
+        }
+        
         $query="
         SELECT DISTINCT u1.username,p1.postLink,p1.postID,p1.postLinkID,p1.postLinkID as post2LinkID,p2.postLink AS post2Link,p2.postID AS post2ID FROM Users AS u1
         INNER JOIN post AS p1 ON u1.userID=p1.userID
         INNER JOIN post AS p2 ON p1.userID=u1.userID
-        WHERE p1.postID<>p2.postID
-        AND p1.previewStatus=1 AND p2.previewStatus=1
-        GROUP BY (u1.username);
+        WHERE p1.postID<>p2.postID 
+        
+        AND p1.previewStatus=1 AND p2.previewStatus=1 GROUP BY (u1.username);
         ";
        
         $stmt=$db->prepare($query);
@@ -30,15 +35,21 @@ public function chrono(Array $arr){
 
 
 public function Basic(Array $arr){
+    $p='';
     try{
         $db=$this->get_connection();
-        $query='
+        $len=count($arr);
+        for($i=0;$i<$len;$i++){
+            $p.='AND userID='.$arr[$i];
+        }
+        $query="
             SELECT username,imageFilePath,imageFileName,imageFilePath as image2FilePath,imageFileName as image2FileName FROM Users
             INNER JOIN Post ON postID
             INNER JOIN Image ON imageID
             INNER JOIN PostImage ON imageID
-            WHERE p1.previewStatus=true AND p2.previewStatus=true ;
-        ';
+            WHERE p1.previewStatus=true AND p2.previewStatus=true 
+            $p;
+        ";
         $stmt=$db->prepare($query);
         $stmt->execute();
         return $stmt->fetchall();
