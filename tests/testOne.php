65cdf78d3be7a0a33fbbda9586ed8ebdfb2047a9
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-07-08 17:54:03 +0200

    pdates

--- tests/testOne.php
+++ tests/testOne.php
@@ -1,9 +1,37 @@
 <?php declare(strict_types=1);
+// reqire 'vendor/phpunit'
 
-namespace Tests;
 use PHPUnit\Framework\TestCase;
 
-class Users{
+$c=curl_init();
+curl_setopt($c,CURLOPT_URL, 'localhost:8000/upload_post');
+$data=[
+	"userID"=>1,
+	"username"=>'saule',
+	"categoryName"=>'',
+	"location"=>'thohoyandou unit D',
+	"caption"=>'happy people',
+	"img"=>turn_image_to_base64('../userProfiles/saule/1cmi6TEG.png'),
+	"collaborators"=>[]
 
+];
+$f_data=json_encode($data);
+curl_setopt($c,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($c,CURLOPT_POST,true);
+curl_setopt($c,CURLOPT_POSTFIELDS,$f_data);
+curl_setopt($c,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+
+$rep=curl_exec($c);
+curl_close($c);
+
+//takesin image path then turn the image to a 64 bit stirng represantation
+function turn_image_to_base64($path){
+
+	$data=file_get_contents($path);
+	$srting64=base64_encode($data);
+	return $srting64;
 }
+
+echo $rep;
+print_r($rep);
 ?>
