b668c134b3b3fe9ca1c2732b9d230b1625696ffe
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-02 03:04:31 +0200

    update

--- php/Video.php
+++ php/Video.php
@@ -11,7 +11,14 @@ class Video{
 	public function __construct(){
 
 	}
-	
+	public function set_id(int $id){}
+	public function set_size($id){}
+	public function set_width($id){}
+	public function set_height($id){}
+	public function set_filename($id){}
+	public function set_dateMade($id){}
+	public function set_timeMade($id){}
+	public function set_status($id){}
 }
 
 ?>
--- php/VideoDB.php
+++ php/VideoDB.php
@@ -7,7 +7,27 @@ class VideoDB extends Database{
 		$this->video=$video;
 	}
 
-	public function addVideo(){}
+	public function addVideo(){
+		$db=$this->db;
+		try{
+			$query='
+					INSERT INTO Video
+					VALUES(:filename,:size,:type,:dateMade,:timeMade,:status)
+			';
+			$statement=$db->prepare($query);
+			$statement->bindValue(':filename',$this->video->get_filename());
+			$statement->bindValue(':size',$this->video->get_size());
+			$statement->bindValue(':type',$this->video->get_type());
+			$statement->bindValue(':dateMade',$this->video->get_dateMade());
+			$statement->bindValue(':timeMade',$this->video->get_timeMade());
+			$statement->bindValue(':status',$this->video->get_status());
+			$statement->execute();
+			$id=$statement->db2_last_insert_id($db);
+			$this->video->set_id($id);
+		}catch(PDOException $err){
+			return $err;
+		}
+	}
 	public function deleteVideo(){}
 
 	public function getVideos(){}
