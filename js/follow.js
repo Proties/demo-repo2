6a60bd2d520e895ea342242b08830ce6bc4642b5
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-02 20:54:26 +0200

    updates

---
+++ js/follow.js
@@ -0,0 +1,65 @@
+"strict"
+export class Follow{
+	constructor(user,influencer){
+		this._user=user;
+		this._influencer=influencer;
+	}
+	set user(s){
+		this._user=s;
+	}
+	get user(){
+		return this._user;
+	}
+	set influencer(s){
+		this._influencer=s;
+	}
+	get influencer(){
+		return this._influencer;
+	}
+	sendFollowTOServer(){
+		try{
+			let xml=new XMLHttpRequest();
+			xml.open('POST','/profile');
+			xml.setRequestHeader('Content-type','x/application-form-urlencoded');
+			xml.send('action=follow&influencerID='+influencer.id);
+		}catch(err){
+			console.log(err)
+		}
+	}
+}
+export class UnFollow{
+	constructor(user,influencer){
+		this._user=user;
+		this._influencer=influencer;
+	}
+	set user(s){
+		this._user=s;
+	}
+	get user(){
+		return this._user;
+	}
+	set influencer(s){
+		this._influencer=s;
+	}
+	get influencer(){
+		return this._influencer;
+	}
+	sendUnFollowTOServer(){
+		try{
+			let xml=new XMLHttpRequest();
+			xml.open('POST','/profile');
+			xml.setRequestHeader('Content-type','x/application-form-urlencoded');
+			xml.send('action=unfollow&influencerID='+influencer.id);
+		}catch(err){
+			console.log(err)
+		}
+	}
+}
+class FollowingList{
+	constructor(){}
+	get_following_list(){}
+}
+class FollowerList{
+	constructor(){}
+	get_follower_list(){}
+}
