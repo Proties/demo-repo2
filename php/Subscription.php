0ef5d0cbaa2a792dfd74c9379fce0e853bd44ac4
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-09 11:55:42 +0200

    upadtd

--- php/Subscription.php
+++ php/Subscription.php
@@ -13,4 +13,17 @@ class Subscription{
 	}
 }
 
+$item=[
+"type"=>"subscription.free-trial",
+"token"=>"dc0521d3-55fe-269b-fa00-b647310d760f",
+"initial_amount"=>0,
+"amount"=>10000,
+"next_run"=>"2021-03-30",
+"frequency"=>"3",
+"item_name"=>"Test Item",
+"item_description"=>"A test product",
+"name_first"=>"John",
+"name_last"=>"Doe",
+"email_address"=>"john@example.com"
+];
 ?>
