e9cf907c20706e6cac8492f924f33d6e9e86df11
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-17 23:10:56 +0200

    updates

---
+++ js/handleTemplates.js
@@ -0,0 +1,5 @@
+"strict";
+//get all templates 
+
+//get return message from cookie
+//	if there is an error write down the error else add template to list
--- php/uploadTemplate.php
+++ php/uploadTemplate.php
@@ -1,76 +1,82 @@
 <?php
+session_start();
+use Insta\Template\Template;
+use Insta\Database\Template\TemplateDB;
+use Insta\Users\User;
+use Insta\Database\Users\UserDB;
+use Insta\Image\Image;
+use Insta\Database\Image\ImageDB;
+use Insta\Databases\Database;
+$data=[];
+$errorMessages=[];
+$user=new Users();
+$temp=new Template();
+$image=new Image();
+$db=Database::get_connection();
 try {
-    // Check if a file was uploaded
-    if($_POST['action']=='updateTemplate'){
-
-        
+    
+    $db->beginTransaction();
+    if(empty($_POST['templateName'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
     }
-    if($_FILES['templateFiles']==null){
-    	throw new Exception('no file was provided');
+    if(empty($_POST['templatePrice'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
     }
-    if ($_FILES['templateFiles']['error'] == UPLOAD_ERR_OK) {
-        
-        // File details
-        $fileName = $_FILES['templateFiles']['name'];
-        $fileType = $_FILES['templateFiles']['type'];
-        $fileSize = $_FILES['templateFiles']['size'];
-        $tmpName = $_FILES['templateFiles']['tmp_name'];
-
-        // Validate file size
-        $maxSize = 5 * 1024 * 1024; // 5MB
-        if ($fileSize > $maxSize) {
-            throw new Exception('File exceeds maximum size of 5MB.');
-        }
-
-        // Move uploaded file to desired location
-        $uploadDir = './uploads/';
-        if (!is_dir($uploadDir)) {
-            mkdir($uploadDir, 0777, true);
-        }
-        $newFilePath = $uploadDir . $fileName;
-
-        // Prevent overwriting existing files
-        if (file_exists($newFilePath)) {
-            throw new Exception('File already exists. Please choose a different name.');
-        }
-
-        // Move uploaded file
-        if (!move_uploaded_file($tmpName, $newFilePath)) {
-            throw new Exception('Failed to move uploaded file.');
-        }
-
-        echo "File uploaded successfully!";
-    } else {
-        // Handle upload errors
-        switch ($_FILES['templateFiles']['error']) {
-            case UPLOAD_ERR_INI_SIZE:
-                throw new Exception('File exceeds maximum size allowed by PHP.');
-                break;
-            case UPLOAD_ERR_FORM_SIZE:
-                throw new Exception('File exceeds maximum size specified in HTML form.');
-                break;
-            case UPLOAD_ERR_PARTIAL:
-                throw new Exception('File was only partially uploaded.');
-                break;
-            case UPLOAD_ERR_NO_FILE:
-                throw new Exception('No file was uploaded.');
-                break;
-            case UPLOAD_ERR_NO_TMP_DIR:
-                throw new Exception('Missing temporary folder.');
-                break;
-            case UPLOAD_ERR_CANT_WRITE:
-                throw new Exception('Failed to write file to disk.');
-                break;
-            case UPLOAD_ERR_EXTENSION:
-                throw new Exception('A PHP extension stopped the file upload.');
-                break;
-        }
+    if(empty($_POST['templateType'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
     }
+    if(empty($_FILES['templateImage'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
+    }
+    if(empty($_FILES['templateHtml'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
+    }
+    if(empty($_FILES['templateCss'])){
+        $errorMessages['errTemplateName'][]='template name not valid';
+    }
+    $errorLen=count($errorMessages);
+    if($errorLen>1){
+        throw new Exception('there are issues');
+    }
+
+    $temp->set_name($_POST['templateName']);
+    $temp->set_price($_POST['templatePrice']);
+    $temp->set_type($_POST['templateType']);
+    $temp->set_html($_FILES['templateHtml']['name']);
+    $temp->set_css();
+    $temp->set_image($_FILES['templateImage']);
+    $temp->set_dateMade();
+    $temp->set_timeMade();
+
+    $image=new Image();
+    $imageDB=new ImageDB($image);
+    $tempDB=new TemplateDB($temp);
+    $tempDB->set_db($db);
+    $imageDB->set_db($db);
+
+    $status=$tempDB->addTemplate();
+
+    if($status==false){
+        throw new Exception('something went wrong writing to database');
+    }
+    $imageDB->load_image();
+    if(){
+        move_uploaded_file($temp->get_filename(), destination);
+    }
+    if(){
+        move_uploaded_file($temp->get_filename(), destination);
+    }
+    if(){
+        move_uploaded_file($temp->get_filename(), destination);
+    }
+    $db->commit();
     $data['status']='success';
-    setcookie('uploadTemplate',json_encode($data),time()+(300*10),'/')
 } catch (Exception $e) {
+    $db->rollBack();
     $data['status']='failed';
     $data['error']=$e->getMessage();
-    setcookie('uploadTemplate',json_encode($data),time()+(300*10),'/')
+
+   
 
 }
+ setcookie('uploadTemplate',json_encode($data),time()+(30*10),'/');
