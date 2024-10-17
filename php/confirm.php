c0cce609e243566abfc6ad14d50bbbbee63bb4e8
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-17 15:45:24 +0200

    updates

--- php/confirm.php
+++ php/confirm.php
@@ -1,18 +1,27 @@
 <?php 
+use Insta\Users\User;
+use Insta\Database\Users\UserDB;
+use Insta\Subscription\Subscription;
+use Insta\Database\Subscription\SubscriptionDB;
 
-
-
-$subcription->set_type($type);
+$user=new Users();
+$order=new Order();
+$subcription=new Subscription();
+$token;
+$nextRun;
+$intialAmount;
+$frequency;
+$subcription->set_type($order->get_type());
 $subcription->set_initial_amount($intialAmount);
 $subcription->set_token($token);
-$subcription->set_amount($amount);
+$subcription->set_amount($order->get_amount());
 $subcription->set_next_run($nextRun);
 $subcription->set_frequency($frequency);
-$subcription->set_item_name($itemName);
-$subcription->set_item_description($itemDescription);
-$subcription->set_name_first($firstName);
-$subcription->set_name_last($lastName);
-$subcription->set_email_address($email);
+$subcription->set_item_name($order->get_item());
+$subcription->set_item_description($order->get_description());
+$subcription->set_name_first($user->get_firstName());
+$subcription->set_name_last($user->get_lastName());
+$subcription->set_email_address($user->get_email());
 
 $subdb=new SubscriptionDB($subcription);
 $subdb->addSubscription();
