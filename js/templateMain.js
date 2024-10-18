d96b268782a69c7b6d650e6fc4b11a62f58c21f5
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-10-18 03:02:11 +0200

    updates

--- js/templateMain.js
+++ js/templateMain.js
@@ -37,9 +37,47 @@ class TemplatePicker{
 	}
 	get_templates(){
 		try{
+			let xml=new XMLHttpRequest();
+			xml.open('POST','/get_templates');
+			xml.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xml.onload=function(){
+				console.log('======== sending tmeplates====');
+				console.log(this.responseText);
+				let bigContainer=document.getElementsByClassName('templatewrapper')[0];
+				let data=JSON.parse(this.responseText);
+				let len=data.templateList.length;
+				for(let t=0;t<len;t++){
+					let cont=document.createElement('div');
+					let contTwo=document.createElement('div');
+					let contThree=document.createElement('div');
+					let type=document.createElement('span');
+					let typeTxt=document.createTextNode(data.templateList[t].type);
+					let price;
+					let image=document.createElement('img');
+					let name=document.createElement('span');
+					let nameTxt=document.createTextNode(data.templateList[t].name);
+					let id;
+					cont.setAttribute('class','template-container');
+					contTwo.setAttribute('class','template-case');
+					contThree.setAttribute('class','template');
+					img.setAttribute('class','template-img');
+					name.setAttribute('class','Template-label');
+					img.setAttribute('src',data.templateList[t].image);
 
+					type.append(typeTxt);
+					name.append(nameTxt);
 
-
+					cont.append(type);
+					cont.append(image);
+					cont.append(name);
+					
+					contTwo.append(cont);
+					contThree.append(contTwo);
+					bigContainer.append(contThree);
+				}
+				
+			}
+			xml.send();
 			parentContainer.append();
 		}catch(err){
 			console.log(err);
