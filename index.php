757f63e95b541b4e829bf4b12b0b99d54df4c567
Rotondwa Nemutavhani <rotondwanemutavhani@Rotondwas-MacBook-Air.local>
2024-11-08 13:56:31 +0200

    updates

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 date_default_timezone_set('Africa/Johannesburg');
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('log_errors', 1);
+ini_set('error_log', 'error.log');
 try{
     require_once 'php/database.php';
 
