c087af0042c467bd3233d307dd91c3214a5e823d
Proties <124195018+Proties@users.noreply.github.com>
2025-04-28 09:57:51 +0200

    Git update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import './Css/App.css'
+import Home from './Pages/FYP'
+import Login from './Pages/LoginPage'
+import Signin from './Pages/SigninPage'
+import Profile from './Pages/PersonalProfile'
+import ProfileSetUp from './Pages/ProfileSetup'
+import SearchPage from './Pages/SearchPage'
+import PickTemplate from './Pages/PickTemplate'
+import Payment from './Pages/PaymentPage'
+import TemplateA from './Pages/Templates/TemplateA'
+import TemplateB from './Pages/Templates/TemplateB'
+import {Routes, Route} from 'react-router-dom'
+import NavBar from './Components/Navbar'
+import Footer from './Components/Footer'
+
 function App() {
-    return (
-      <div>
-        <h1>Hello, React!</h1>
-      </div>
-    );
-  }
-  
-  export default App;
-  
+  return (
+      <MovieProvider>
+        <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signin" element={<Signin />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/ProfileSetUp" element={<ProfileSetUp />} />
+          <Route path="/Search" element={<SearchPage />} />
+          <Route path="/TemplatePicker" element={<PickTemplate />} />
+          <Route path="/Payment" element={<Payment />} />
+          <Route path="/TemplateA" element={<TemplateA />} />
+          <Route path="/TemplateB" element={<TemplateB />} />          
+        </Routes>
+      </main>
+      <Footer />
+      </MovieProvider>
+    )
+}
+
+
+export default App
